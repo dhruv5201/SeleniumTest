40158450bf894c629422ce001a3b76790e413d0d
dhruv5201 <dhrubajitdas@gmail.com>
2019-12-10 23:52:36 +0530

    FisrtCommit.

---
+++ src/test/java/com/selenium/test/SampleSelenium/E2Etest.java
@@ -0,0 +1,49 @@
+package com.selenium.test.SampleSelenium;
+
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+public class E2Etest {
+	WebDriver driver;
+  @Test
+  public void Login() {
+	  driver.get("https://demo.testfire.net/login.jsp");
+	  driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	  driver.findElement(By.xpath("//input[@id='uid']")).sendKeys("Admin");
+	  driver.findElement(By.xpath("//input[@id='passw']")).sendKeys("Admin");
+	  driver.findElement(By.xpath("//input[@name='btnSubmit']")).click();
+	  SoftAssert sa=new SoftAssert();
+	  if (driver.findElement(By.xpath("//font[contains(text(),'Sign Off')]")).isDisplayed()) {
+		  String ValidationString= driver.findElement(By.xpath("//font[contains(text(),'Sign Off')]")).getText();
+		  
+		  sa.assertEquals(ValidationString, "Sign Off");
+	  }
+	  else {
+		  System.out.println("Can not validate Login Success as Validation eliment not visisble..!");
+	  }
+	  sa.assertAll();
+	  
+	  
+	  
+	  
+  }
+  @BeforeTest
+  public void beforeTest() {
+	  System.setProperty("webdriver.gecko.driver", "src/main/geckodriver.exe");
+	  driver = new FirefoxDriver();
+	  
+  }
+  @AfterTest
+  public void closeBrowser() {
+	  driver.quit();
+  }
+
+}
